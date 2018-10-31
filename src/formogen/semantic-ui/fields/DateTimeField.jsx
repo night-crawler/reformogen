aebfb2b1841d1f470fa0c7a5b6f1ce383521b5ac
night-crawler <lilo.panic@gmail.com>
2018-10-31 13:49:28 +0300

    refactored DateTimeField

--- src/formogen/semantic-ui/fields/DateTimeField.jsx
+++ src/formogen/semantic-ui/fields/DateTimeField.jsx
@@ -10,22 +10,6 @@ import { FieldLabel } from '../common/FieldLabel';
 import { ErrorsList } from '../common/ErrorsList';
 
 
-/**
- * Component should receive a value in ISO format
- * @param {object} props
- * @param {string} props.name - internal field name
- * @param {string} props.verbose_name
- * @param {string} props.placeholder
- * @param {string} props.help_text -
- * @param {bool} props.blank - determines if we can leave field unfilled
- * @param {bool} props.null - determines if we can store NULL values
- * @param {bool} props.editable - can we edit the field value?
- * @param {string} props.type - Django ORM field type
- * @param {bool} props.required
- * @param {string} props.value - well ISO-formatted value to pass moment()
- * @returns {XML}
- * @constructor
- */
 DateTimeField.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
@@ -42,38 +26,29 @@ DateTimeField.propTypes = {
   layoutOpts: layoutOptsType,
   locale: PropTypes.string,
 
-  updateProps: PropTypes.func,
+  
+  timeFormat: PropTypes.string,
+  timeIntervals: PropTypes.number,
+  todayButton: PropTypes.string,
+  dateFormat: PropTypes.string,
+  dropdownMode: PropTypes.string,
+
   onChange: PropTypes.func,
+};
+DateTimeField.defaultProps = {
+  timeFormat: 'HH:mm',
+  timeIntervals: 30,
+  todayButton: 'Now',
+  dateFormat: 'YYYY.MM.DD HH:mm',
+  dropdownMode: 'select',
 };
 export function DateTimeField(props) {
   const handleChange = (momentTimeObject, e) => {
-    // proxy event with SUI-React compliant style
     props.onChange(e, {
       name: props.name,
       value: momentTimeObject ? momentTimeObject.toISOString() : null
     });
   };
-
-  let _props = {
-    name: props.name,
-    selected: props.value ? moment(props.value) : null,  // null == no selected value
-    onChange: handleChange,
-    showTimeSelect: true,
-    timeFormat: 'HH:mm',
-    timeIntervals: 60,
-    locale: props.locale,
-    placeholderText: props.placeholder,
-    todayButton: 'Now',
-    // todo: get from momemt
-    dateFormat: 'YYYY.MM.DD HH:mm',
-    showYearDropdown: true,
-    showMonthDropdown: true,
-    dropdownMode: 'select',
-  };
-
-  if (_.isFunction(props.updateProps)) {
-    _props = props.updateProps(_props, props);
-  }
 
   return (
     <Form.Field
@@ -83,11 +58,27 @@ export function DateTimeField(props) {
       error={ !_.isEmpty(props.errors) }
     >
       <FieldLabel { ...props } />
-      <DatePicker { ..._props } />
-      {
-        !props.helpTextOnHover
-          ? <span className='help-text'>{ props.help_text }</span>
-          : ''
+      <DatePicker 
+        name={ props.name }
+        selected={ props.value ? moment(props.value) : null }  // null == no selected value
+        onChange={ handleChange }
+        showTimeSelect={ true }
+        
+        locale={ props.locale }
+        placeholderText={ props.placeholder }
+        timeFormat={ props.timeFormat }
+        timeIntervals={ props.timeIntervals }
+        todayButton={ props.todayButton }
+        // TODO: get from moment
+        dateFormat={ props.dateFormat }
+        dropdownMode={ props.dropdownMode }
+        
+        showYearDropdown={ true }
+        showMonthDropdown={ true }
+      />
+      { !props.helpTextOnHover
+        ? <span className='help-text'>{ props.help_text }</span>
+        : ''
       }
       <ErrorsList messages={ props.errors } />
     </Form.Field>
---
+++ stories/DateTimeField.stories.js
@@ -0,0 +1,30 @@
+import 'react-datepicker/dist/react-datepicker.css';
+import React from 'react';
+import { text, boolean, number } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import { DateTimeField } from '~/formogen/semantic-ui/fields/DateTimeField';
+
+import { FormDecorator } from './FormDecorator';
+import { errorArray } from './ErrorMessages.sample';
+
+const stories = storiesOf('Fields|DateTimeField', module).addDecorator(FormDecorator);
+
+
+stories.add('default', () => {
+  return (
+    <DateTimeField 
+      type='DateField'
+      placeholder={ text('placeholder', 'some placeholder') }
+      editable={ boolean('editable', true) }
+      help_text={ text('help_text', 'some help text') }
+      name={ text('name', 'dateTimeField') }
+      verbose_name={ text('verbose_name', 'verbose name of the field') }
+      layoutOpts={ { width: number('layoutOpts.width', 4, { range: true, min: 1, max: 16 }) } }
+      onChange={ action('onChange') }
+      value={ text('value', '2018-10-09T21:00:00.000Z') }
+      errors={ errorArray }
+    />
+  );
+});
