f0a182f59f4e94aa901033fe321f1f6a6d38c698
night-crawler <lilo.panic@gmail.com>
2018-11-08 14:16:37 +0300

    fixed AutocompleteChoiceField, see more
    https://github.com/JedWatson/react-select/issues/1151

--- src/formogen/semantic-ui/fields/AutocompleteChoiceField.jsx
+++ src/formogen/semantic-ui/fields/AutocompleteChoiceField.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import _ from 'lodash';
+import { isEmpty } from 'lodash';
 import { Form } from 'semantic-ui-react';
 import Select from 'react-select';
 
@@ -32,35 +32,41 @@ AutocompleteChoiceField.propTypes = {
   helpTextOnHover: PropTypes.bool,
   layoutOpts: layoutOptsType,
 
+  getOptionLabel: PropTypes.func,
+  getOptionValue: PropTypes.func,
+
   onChange: PropTypes.func,
 };
 AutocompleteChoiceField.defaultProps = {
   choices: [],
+  getOptionLabel: ([ id, name ]) => name,
+  getOptionValue: ([ id ]) => id,
 };
 export function AutocompleteChoiceField(props) {
-  const handleChange = newVal => {
-    props.onChange(
-      null,
-      { name: props.name, value: newVal ? newVal.value : null }
-    );
-  };
+  const handleChange = ([ id ]) => props.onChange(
+    null,
+    { name: props.name, value: id !== undefined ? id : null }
+  );
 
   return (
     <Form.Field
       required={ props.required }
       disabled={ !props.editable }
       width={ props.layoutOpts.width }
-      error={ !_.isEmpty(props.errors) }
+      error={ !isEmpty(props.errors) }
     >
       <FieldLabel { ...props } />
       <Select 
+        isMulti={ false }
         clearable={ !props.required }
         name={ props.name }
-        value={ props.value }
+        value={ props.choices.filter(([ id, name ]) => props.value === id ) }
         placeholder={ props.placeholder }
-        options={ makeReactSelectOptions(props.choices) }
+        options={ props.choices }
         onChange={ handleChange }
-        inputProps={ { type: 'react-type' } }
+
+        getOptionLabel={ props.getOptionLabel }
+        getOptionValue={ props.getOptionValue }
       />
       { !props.helpTextOnHover 
         ? <span className='help-text'>{ props.help_text }</span>
