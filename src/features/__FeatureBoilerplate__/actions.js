bef2deaa6a66e8cf2b364e78db7d69fb92565355
night-crawler <lilo.panic@gmail.com>
2018-12-05 16:36:16 +0300

    removed useless stuff

--- src/features/__FeatureBoilerplate__/actions.js
+++
@@ -1,5 +0,0 @@
-import { FETCH_FEATURE } from './constants';
-
-export const fetchFeature = () => ({
-  type: FETCH_FEATURE,
-});
--- src/features/__FeatureBoilerplate__/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-
-
-export const feature = state => state.feature;
-
-/**
- * state.feature.data
- */
-export const data = createSelector(feature, feature => feature.data);
